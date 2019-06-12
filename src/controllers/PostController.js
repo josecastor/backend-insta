639710e67c0466bd7ec921038b7d6a60f82a2670
Jose Castor <jose.neto@totvs.com.br>
2019-06-12 07:34:17 -0700

    Resize image for application mobile

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,4 +1,7 @@
 const Post = require('../models/Post');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
     async index(req, res) {
@@ -10,7 +13,19 @@ module.exports = {
         const { author, place, description, hashtags } = req.body;
         const { filename: image } = req.file;
 
-        const post = await Post.create({ author, place, description, hashtags, image });
+        const [ name ] = image.split('.');
+        const fileName = `${name}.jpg`;
+
+        await sharp(req.file.path)
+            .resize(500)
+            .jpeg({ quality: 70 })
+            .toFile(
+                path.resolve(req.file.destination, 'resized', fileName)
+            )
+        
+        fs.unlinkSync(req.file.path);
+
+        const post = await Post.create({ author, place, description, hashtags, image: fileName });
 
         return res.json({ post });
     }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect(`mongodb+srv://insta:insta@cluster0-ekyid.mongodb.net/test?retryWrites=true&w=majority`, {
     useNewUrlParser: true
 })
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
